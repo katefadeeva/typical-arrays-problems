5312b8ae93e892962b60f07e02fe21e384660406
Kate Fd <katesfadeeva@gmail.com>
2020-03-03 18:13:28 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+      } else {
+       return Math.min.apply(null, array);
+      }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+      } else {
+        return Math.max.apply(null, array);
+      }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+      } else {
+        let k = 0;
+        for (let i = 0; i < array.length; i++) {
+          k += array[i];
+          } return k/array.length;
+        } 
 }
